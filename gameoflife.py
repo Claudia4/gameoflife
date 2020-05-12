fb4bccca843c198bd4e224cd8d7aef198dc1d8b0
annikahe <alhennes97@gmail.com>
2020-05-12 22:14:04 +0200

    implemented user input

--- gameoflife.py
+++ gameoflife.py
@@ -4,82 +4,117 @@ import matplotlib.pyplot as plt
 xdim = 101
 ydim = 82
 
-left = np.zeros([xdim,ydim])
-right= np.zeros(left.shape)
+left = np.zeros([xdim, ydim])
+right = np.zeros(left.shape)
 
-#todo: get user input
-#initialise left with the desired pattern
 
-#blinker
-left[20:23,10]=1
+# initialise left with the desired pattern
 
-#glider
-left[30:33,50]=1
-left[32,51]=1
-left[31,52]=1
+# blinker
+def init_blinker(left):
+    left[20:23, 10] = 1
+    return left
 
-#glider that is close to the boundary so you can check the torus property
-#left[90:93,20]=1
-#left[92,21]=1
-#left[91,22]=1
 
-#r-pentomino
-left[50:53,50]=1
-left[50,51]=1
-left[51,49]=1
+# glider
+def init_glider(left):
+    left[30:33, 50] = 1
+    left[32, 51] = 1
+    left[31, 52] = 1
+    return left
+
+
+# glider that is close to the boundary so you can check the torus property
+# left[90:93,20]=1
+# left[92,21]=1
+# left[91,22]=1
+
+
+# r-pentomino
+def init_r_pentomino(left):
+    left[50:53, 50] = 1
+    left[50, 51] = 1
+    left[51, 49] = 1
+    return left
 
 
 # Gosper's Glidergun
-pos_left_stopblock = (5, 10)
-x, y = pos_left_stopblock
-left[x:(x + 2), y:(y + 2)] = 1  # left stop block
-# left side of main part
-left[x:(x + 3), (y + 10)] = 1
-left[x - 1, y + 11] = 1
-left[x + 3, y + 11] = 1
-left[x - 2, (y + 12):(y + 14)] = 1
-left[x + 4, (y + 12):(y + 14)] = 1
-left[x + 1, y + 14] = 1
-left[x - 1, y + 15] = 1
-left[x + 3, y + 15] = 1
-# left side of main part
-left[x:(x+3), y + 16] = 1
-left[x + 1, y + 17] = 1
-left[(x-2):(x+1), (y + 20):(y + 22)] = 1
-left[x - 3, y + 22] = 1
-left[x + 1, y + 22] = 1
-left[(x - 4):(x - 2), y + 24] = 1
-left[(x + 1):(x + 3), y + 24] = 1
-# right stop block
-left[(x - 2):x, (y + 34):(y + 36)] = 1
-
-
+def init_gospers_glidergun(left):
+    pos_left_stopblock = (5, 10)
+    x, y = pos_left_stopblock
+    left[x:(x + 2), y:(y + 2)] = 1  # left stop block
+    # left side of main part
+    left[x:(x + 3), (y + 10)] = 1
+    left[x - 1, y + 11] = 1
+    left[x + 3, y + 11] = 1
+    left[x - 2, (y + 12):(y + 14)] = 1
+    left[x + 4, (y + 12):(y + 14)] = 1
+    left[x + 1, y + 14] = 1
+    left[x - 1, y + 15] = 1
+    left[x + 3, y + 15] = 1
+    # left side of main part
+    left[x:(x + 3), y + 16] = 1
+    left[x + 1, y + 17] = 1
+    left[(x - 2):(x + 1), (y + 20):(y + 22)] = 1
+    left[x - 3, y + 22] = 1
+    left[x + 1, y + 22] = 1
+    left[(x - 4):(x - 2), y + 24] = 1
+    left[(x + 1):(x + 3), y + 24] = 1
+    # right stop block
+    left[(x - 2):x, (y + 34):(y + 36)] = 1
+    return left
 
 
 '''Update the matrix a, given the current state b, using the Game of Life rules'''
-def update(a,b):
-	for x in range(xdim):
-		for y in range(ydim):
-			if(b[x,y]==0):
-				if(b[x-1,y-1]+b[x-1,y]+b[x-1, (y+1)%ydim]+b[x,y-1]+b[x,(y+1)%ydim]+b[(x+1)%xdim,y-1]+b[(x+1)%xdim,y]+b[(x+1)%xdim,(y+1)%ydim] == 3):
-					a[x,y]=1
-				else:
-					a[x,y]=0
-			else: # b[x,y]=1
-				if(b[x-1,y-1]+b[x-1,y]+b[x-1, (y+1)%ydim]+b[x,y-1]+b[x,(y+1)%ydim]+b[(x+1)%xdim,y-1]+b[(x+1)%xdim,y]+b[(x+1)%xdim,(y+1)%ydim] == 2 or b[x-1,y-1]+b[x-1,y]+b[x-1, (y+1)%ydim]+b[x,y-1]+b[x,(y+1)%ydim]+b[(x+1)%xdim,y-1]+b[(x+1)%xdim,y]+b[(x+1)%xdim,(y+1)%ydim] == 3):
-					a[x,y]=1
-				else:
-					a[x,y]=0
+
+
+def update(a, b):
+    for x in range(xdim):
+        for y in range(ydim):
+            if (b[x, y] == 0):
+                if (b[x - 1, y - 1] + b[x - 1, y] + b[x - 1, (y + 1) % ydim] + b[x, y - 1] + b[x, (y + 1) % ydim] + b[
+                    (x + 1) % xdim, y - 1] + b[(x + 1) % xdim, y] + b[(x + 1) % xdim, (y + 1) % ydim] == 3):
+                    a[x, y] = 1
+                else:
+                    a[x, y] = 0
+            else:  # b[x,y]=1
+                if (b[x - 1, y - 1] + b[x - 1, y] + b[x - 1, (y + 1) % ydim] + b[x, y - 1] + b[x, (y + 1) % ydim] + b[
+                    (x + 1) % xdim, y - 1] + b[(x + 1) % xdim, y] + b[(x + 1) % xdim, (y + 1) % ydim] == 2 or b[
+                    x - 1, y - 1] + b[x - 1, y] + b[x - 1, (y + 1) % ydim] + b[x, y - 1] + b[x, (y + 1) % ydim] + b[
+                    (x + 1) % xdim, y - 1] + b[(x + 1) % xdim, y] + b[(x + 1) % xdim, (y + 1) % ydim] == 3):
+                    a[x, y] = 1
+                else:
+                    a[x, y] = 0
+
 
 '''Show/plot the current state of the Game, as given in matrix m'''
+
+
 def showme(m):
-	plt.matshow(m, fignum=0, cmap='binary')
-	plt.show(block=False)
-	plt.pause(0.05)
+    plt.matshow(m, fignum=0, cmap='binary')
+    plt.show(block=False)
+    plt.pause(0.05)
 
 
-while(True):
-	showme(left)
-	update(right,left)
-	showme(right)
-	update(left,right)
+print("Patterns:\n"
+      "(1) Blinker\n"
+      "(2) Glider\n"
+      "(3) R Pentomino\n"
+      "(4) Glidergun")
+choice = int(input("Choose one of the above patterns using the respective number:"))
+if choice == 1:
+    left = init_blinker(left)
+elif choice == 2:
+    left = init_glider(left)
+elif choice == 3:
+    left = init_r_pentomino(left)
+elif choice == 4:
+    left = init_gospers_glidergun(left)
+else:
+    print("Input not valid.")
+
+while True:
+    showme(left)
+    update(right, left)
+    showme(right)
+    update(left, right)
